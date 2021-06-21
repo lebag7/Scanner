5f4cfd4ffeb7101095fd81fe5f8f978a21856713
Jakob <nicole.jakob@accenture.com>
2021-06-21 17:22:29 +0200

    renaming files and project as well as shorten the movie list

---
+++ src/com/accenture/gamechangers/Scanner/MovieList.java
@@ -0,0 +1,9 @@
+package com.accenture.gamechangers.Scanner;
+
+public class MovieList {
+	public static void main(String[] args) throws Exception {
+
+		FileReader myMovies = new FileReader();
+		System.out.println(myMovies);
+	}
+}
--- src/com/accenture/surfershells/GuessTheMovie/GuessTheMovie.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.surfershells.GuessTheMovie;
-
-public class GuessTheMovie {
-	public static void main(String[] args) throws Exception {
-
-		Game myGame = new Game();
-		System.out.println(myGame);
-	}
-}
