c33cb67b88c6b1120a64140fb45ae762fa655e66
Jakob <nicole.jakob@accenture.com>
2021-06-21 16:31:02 +0200

    first commit

---
+++ src/com/accenture/surfershells/GuessTheMovie/GuessTheMovie.java
@@ -0,0 +1,9 @@
+package com.accenture.surfershells.GuessTheMovie;
+
+public class GuessTheMovie {
+	public static void main(String[] args) throws Exception {
+
+		Game myGame = new Game();
+		System.out.println(myGame);
+	}
+}
